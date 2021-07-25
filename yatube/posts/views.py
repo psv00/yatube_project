aa2c2b5410f8c4bcfca091033db723171d11efc6
psv00 <psv00303@yandex.ru>
2021-07-26 00:17:29 +0500

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse('Главная страниц')
+    return HttpResponse('Главная страница')
 
+def group_posts(request, slug):
+    return HttpResponse('ololo')    
 
-def group_posts(request):
-    return HttpResponse('По группам')
